5c4db0b42acb09c0f0f84dea564288d3c4012e9d
shobhnik13 <shobhnikw@gmail.com>
2022-12-07 12:21:17 +0530

    initial commit

---
+++ src/components/main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const main = () => {
+  return (
+    <main>
+      <h1 className='mainheading'>Fun facts about react</h1>
+      <ul className='mainlist'>
+        <li>Was first released in 2013</li>
+        <li>was originally created by jordan walke</li>
+        <li>has 100k stars on github</li>
+        <li>is maintained by facebook</li>
+         
+      </ul>
+    </main>
+  )
+}
+
+export default main
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav>
+      <img src="../images/react.png" />
+    <h3>react facts</h3>
+    <h4>react course-project 1</h4>
+    </nav>
+  )
+}
+
+export default navbar
